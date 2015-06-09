1d9dd32dc81bf997f801481aff6af178aef180d0
Brendon Lees <brendon1555@gmail.com>
2015-06-09 13:55:54 +1000

    Added argument support and broke server into methods

--- my_client.py
+++ my_client.py
@@ -1,31 +1,25 @@
 import socket
 import json
 
+#create a socket
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
+#define address of server
 server_address = ("localhost", 8080)
 
 print "Connecting to %s port %s" % server_address
-
+#connect to server
 sock.connect(server_address)
 
-user_text = raw_input("Enter a request: ")
-
-user_json = input("Enter some json: ")
-
 try:
+    #take string input from user
+    user_text = raw_input("Enter a request: ")
 
+    #define request and send to server as json
     request = {"request": user_text}
     print "Sending ", request
     sock.sendall(json.dumps(request))
-    result = sock.recv(1024)
-
-    print result
-
-
-    request = user_json
-    print "Sending ", request
-    sock.sendall(json.dumps(request))
+    #get result back from server
     result = sock.recv(1024)
 
     print result
@@ -70,5 +64,9 @@ try:
 
     print result
 
+except SyntaxError, e:
+    print "Client: Syntax Error"
+
 finally:
+    #close socket
     sock.close()
--- my_server.py
+++ my_server.py
@@ -1,59 +1,99 @@
 import socket
 import json
-
-#Create a socket
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-#define address for socket to be used on
-server_address = ("localhost", 8080)
-
-print "Starting on %s port %s" % server_address
-
-#bind socket to address
-sock.bind(server_address)
-
-#start listening for conections, only allow 1 connection
-sock.listen(1)
+import argparse
 
 
-while True:
-    print "Waiting for a connection"
-    #accept connections. conn is socket object, addr is address of client
-    conn, addr = sock.accept()
+ip = "localhost"
+port = 8080
 
-    try:
-        print "Connection from ", addr
+sock = socket.socket()
 
+parser = argparse.ArgumentParser()
+parser.add_argument("-i", "--ip", help="Ip to start server on.")
+parser.add_argument("-p", "--port", type=int, help="Port to start server on.")
+args = parser.parse_args()
+
+class MyServer:
+
+    def __init__(self):
+
+        #Create a socket
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+
+    def bind(self):
+        if args.ip:
+            ip = args.ip
+        if args.port:
+            port = args.port
+
+        #define address for socket to be used on
+        server_address = (ip, port)
+
+        print "Starting on %s port %s" % server_address
+
+        #bind socket to address
+        sock.bind(server_address)
+
+
+    def run(self):
+        #start listening for conections, only allow 1 connection
+        sock.listen(1)
         while True:
-            data = conn.recv(1024)
-            print "Received '%s' "% data
+            print "Waiting for a connection"
+            #accept connections. conn is socket object, addr is address of client
+            conn, addr = sock.accept()
+
             try:
-                json_data = json.loads(data)
-                if data:
-                    if (json_data["request"] == "cat") or (json_data["request"] == "dog") or (json_data["request"] == "bear"):
-                        print "Valid request"
-                        print "Sending response back"
-                        conn.sendall("Yes, you sent %s" % json_data["request"])
-                    else:
-                        print "Invalid request"
-                        print "Sending response back"
-                        conn.sendall("No, you sent %s" % json_data["request"])
-                else:
-                    print "No more data from ", addr
-                    break
-            except ValueError, e:
-                print "Invalid Value"
-                conn.sendall("Server: ValueError")
+                print "Connection from ", addr
 
-            except KeyError, e:
-                print "Invalid Key"
-                conn.sendall("Server: KeyError")
+                while True:
+                    #receive data
+                    data = conn.recv(1024)
+                    print "Received '%s' "% data
+                    try:
+                        #read data from json
+                        json_data = json.loads(data)
+                        if data:
+                            #check for valid request
+                            if (json_data["request"] == "cat") or (json_data["request"] == "dog") or (json_data["request"] == "bear"):
+                                print "Valid request"
+                                print "Sending response back"
+                                #send back response as text
+                                conn.sendall("Yes, you sent %s" % json_data["request"])
+                            #if request is invalid
+                            else:
+                                print "Invalid request"
+                                print "Sending response back"
+                                #send back response as text
+                                conn.sendall("No, you sent %s" % json_data["request"])
+                        else:
+                            print "No more data from ", addr
+                            break
+                    #Check for errors in json        
+                    except ValueError, e:
+                        print "Invalid Value"
+                        #send error type as text to client
+                        conn.sendall("Server: ValueError")
+                    except KeyError, e:
+                        print "Invalid Key"
+                        #send error type as text to client
+                        conn.sendall("Server: KeyError")
+                    except TypeError, e:
+                        print "Invalid Type"
+                        #send error type as text to client
+                        conn.sendall("Server: TypeError")
+            #catch socket errors
+            except socket.error:
+                print "Socket Error"            
+            finally:
+                #close socket
+                conn.close()
 
-            except TypeError, e:
-                print "Invalid Type"
-                conn.sendall("Server: TypeError")
+def main():
+    serv = MyServer()
+    serv.bind()
+    serv.run()
 
-    except socket.error:
-        print "Socket Error"            
-    finally:
-        conn.close()
+if __name__ == "__main__":
+    main()
